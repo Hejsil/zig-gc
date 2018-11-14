7fc4548ba2d912169253add67889f37886252ee4
Jimmi Holst Christensen <jhc@liab.dk>
2018-11-14 14:34:36 +0100

    Now works in release mode

--- src/gc.zig
+++ src/gc.zig
@@ -25,14 +25,14 @@ pub const GcAllocator = struct {
         memory: []u8,
     };
 
-    pub fn init(child_alloc: *mem.Allocator, start: *const u8) GcAllocator {
+    pub inline fn init(child_alloc: *mem.Allocator) GcAllocator {
         return GcAllocator{
             .base = mem.Allocator{
                 .allocFn = alloc,
                 .reallocFn = realloc,
                 .freeFn = free,
             },
-            .start = @ptrCast([*]const u8, start),
+            .start = @ptrCast([*]const u8, @frameAddress()),
             .ptrs = PointerList.init(child_alloc),
         };
     }
@@ -50,6 +50,19 @@ pub const GcAllocator = struct {
     }
 
     pub fn collect(gc: *GcAllocator) void {
+        @noInlineCall(collectNoInline, gc);
+    }
+
+    pub fn collectFrame(gc: *GcAllocator, frame: []const u8) void {
+        gc.mark(frame);
+        gc.sweep();
+    }
+
+    pub fn allocator(gc: *GcAllocator) *mem.Allocator {
+        return &gc.base;
+    }
+
+    fn collectNoInline(gc: *GcAllocator) void {
         const frame = blk: {
             const end = @ptrCast([*]const u8, @frameAddress());
             const i_start = @ptrToInt(gc.start);
@@ -60,15 +73,6 @@ pub const GcAllocator = struct {
             break :blk end[0 .. i_start - i_end];
         };
         gc.collectFrame(frame);
-    }
-
-    pub fn collectFrame(gc: *GcAllocator, frame: []const u8) void {
-        gc.mark(frame);
-        gc.sweep();
-    }
-
-    pub fn allocator(gc: *GcAllocator) *mem.Allocator {
-        return &gc.base;
     }
 
     fn mark(gc: *GcAllocator, frame: []const u8) void {
@@ -165,7 +169,7 @@ const Leaker = struct {
 };
 
 test "gc.collect: No leaks" {
-    var gc = GcAllocator.init(debug.global_allocator, @frameAddress());
+    var gc = GcAllocator.init(debug.global_allocator);
     defer gc.deinit();
     const allocator = gc.allocator();
 
@@ -184,13 +188,13 @@ fn leak(allocator: *mem.Allocator) !void {
 }
 
 test "gc.collect: Leaks" {
-    var gc = GcAllocator.init(debug.global_allocator, @frameAddress());
+    var gc = GcAllocator.init(debug.global_allocator);
     defer gc.deinit();
     const allocator = gc.allocator();
 
     var a = try allocator.create(Leaker{ .l = try allocator.create(Leaker(undefined)) });
     a.l.l = a;
-    try leak(allocator);
+    try @noInlineCall(leak, allocator);
     gc.collect();
 
     debug.assert(gc.findPtr(a) != null);
@@ -199,7 +203,7 @@ test "gc.collect: Leaks" {
 }
 
 test "gc.free" {
-    var gc = GcAllocator.init(debug.global_allocator, @frameAddress());
+    var gc = GcAllocator.init(debug.global_allocator);
     defer gc.deinit();
     const allocator = gc.allocator();
 
